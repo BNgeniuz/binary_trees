10e2773022b7df957ef41abaefec4310062172cd
mwunnam <mwunnam@gmail.com>
2024-03-01 03:14:47 +0000

    correction 13

---
+++ new_13.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - nodes with at least 1 child in a binary tree
+ * @tree: input to count the nodes
+ *
+ * Return: number of nodes counted, or 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1 + binary_tree_nodes(tree->left) +
+			       	binary_tree_nodes(tree->right));
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
