a4812e8d03d21ca881cb6f98048ebf529c74c7a7
mwunnam <mwunnam@gmail.com>
2024-03-01 01:53:33 +0000

    correction on task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,21 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *fresh;
+	binary_tree_t *new_node;
 
 	if (!parent)
 		return (NULL);
 
-	fresh = malloc(sizeof(binary_tree_t));
-	if (!fresh)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
 		return (NULL);
 
-	fresh->n = value;
-	fresh->parent = parent;
-	fresh->left = NULL;
-	fresh->right = parent->right;
-	parent->right = fresh;
-	if (fresh->right)
-		fresh->right->parent = fresh;
-	return (fresh);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
+	parent->right = new_node;
+	if (new_node->right)
+		new_node->right->parent = new_node;
+	return (new_node);
 }
