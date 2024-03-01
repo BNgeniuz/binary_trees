fbe6f542156f1445d23f52feec58f51cb9e517a8
mwunnam <mwunnam@gmail.com>
2024-03-01 03:08:16 +0000

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,39 +8,17 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right))
+	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_size(tree) - binary_tree_leaves(tree));
-}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (binary_tree_size(tree) - binary_tree_leaves(tree));
+	}
 
-/**
- * binary_tree_size - size of a binary tree
- * @tree: input to measure the size
- *
- * Return: size of the tree, or 0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
+	else
+	{
 		return (0);
+	}
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
-
-/**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: ptr input to count the leaves
- *
- * Return: number of leaves, or 0 if tree is NULL
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
