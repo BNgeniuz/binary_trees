258c5e9b8928ece8c76406899d78f9b0d8a8ee92
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-01 19:12:03 -0800

    done with binary tasks

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap
+ * @heap: input ptr root node of the heap to convert
+ * @size: addrs to store the size of the array
+ *
+ * Return: the generated array, NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
